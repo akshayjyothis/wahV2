099695e2d3d171a6a53255e7f50576284820adc9
akshayjyothis <64101639+akshayjyothis@users.noreply.github.com>
2020-09-09 20:09:53 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+//jshint esversion:6
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-items');
+    const hide = document.querySelector('.right-panel');
+    const section = document.querySelector('.section');
+    const body = document.querySelector('body');
+
+
+    burger.addEventListener("click" , () => {
+        nav.classList.toggle("nav-active");
+        burger.classList.toggle("toggle");
+        hide.classList.toggle("active");
+        body.classList.toggle("mob-view");
+    });
+};
+
+navSlide();
